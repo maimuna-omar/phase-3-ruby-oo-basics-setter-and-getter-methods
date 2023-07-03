f56804547c1ba63e98164e7c5417149d1308e162
maimuna omar <maymunaabdi07@gmail.com>
2023-07-04 02:09:22 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,23 @@
 # lib/dog.rb
-class Dog
-    attr_accessor :name, :breed
-  end
+# class Dog
+#     attr_accessor :name, :breed
+#   end
   
+  
+class Dog
+    def name=(name)
+      @name = name
+    end
+  
+    def name
+      @name
+    end
+  
+    def breed=(breed)
+      @breed = breed
+    end
+  
+    def breed
+      @breed
+    end
+  end
